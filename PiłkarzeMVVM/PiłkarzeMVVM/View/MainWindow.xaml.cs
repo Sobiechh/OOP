5645dd47869d3ea1239464883089b4d4c8c10f29
Sobiechx <p.sobieszczyk1@gmail.com>
2020-04-17 22:31:53 +0200

    try

---
+++ PiłkarzeMVVM/PiłkarzeMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PiłkarzeMVVM
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
